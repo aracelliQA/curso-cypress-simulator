e5d1ab6ccd418e69614e7d9cc570fa4e23a9abd0
Aracelli Novaes <aracellieng@gmail.com>
2025-03-06 20:33:51 -0300

    Test suite and test cases

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -15,4 +15,43 @@ describe('Cypress Simulator', () => {
  
   });
 
+  it('Maximize/minimize', () => {
+ 
+  });
+
+  it('Running... state', () => {
+ 
+  });
+
+  it('Accept cookies', () => {
+ 
+  });
+
+  it('Decline cookies', () => {
+ 
+  });
+
+  it('Captcha button states', () => {
+ 
+  });
+
+  it('Captcha error', () => {
+ 
+  });
+
+  it('Run button - enable/disable states', () => {
+ 
+  });
+
+  it('Reset text area on logout and login', () => {
+ 
+  });
+
+  it('Disable Run button on logout and login', () => {
+ 
+  });
+
+  it('No cookies banner on login page', () => {
+ 
+  });
 })
